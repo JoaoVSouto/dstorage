3b90ef2239b1b757b520164db9c7a9c4edf806e5
João Vítor <joaovsoutosantos@gmail.com>
2021-10-05 13:36:13 -0300

    Add upload file function validators

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -23,25 +23,21 @@ contract DStorage {
     constructor() public {}
 
     function uploadFile(string memory _fileHash, uint256 _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+        require(bytes(_fileHash).length > 0);
+
+        require(bytes(_fileType).length > 0);
+
+        require(bytes(_fileDescription).length > 0);
+
+        require(bytes(_fileName).length > 0);
+
+        require(msg.sender != address(0));
+
+        require(_fileSize > 0);
+
         fileCount++;
 
         files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
-
-        // Make sure the file hash exists
-
-        // Make sure file type exists
-
-        // Make sure file description exists
-
-        // Make sure file fileName exists
-
-        // Make sure uploader address exists
-
-        // Make sure file size is more than 0
-
-        // Increment file id
-
-        // Add File to the contract
 
         // Trigger an event
     }   
